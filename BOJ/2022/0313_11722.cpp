95d1c908700fc441812b3aa0de1acb9733f875fd
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-03-15 01:45:43 +0900

    11722. 가장 긴 감소하는 부분 수열
    수열 A가 주어졌을 때, 가장 긴 감소하는 부분 수열을 구하는 프로그램을 작성하시오.
    예를 들어, 수열 A = {10, 30, 10, 20, 20, 10} 인 경우에 가장 긴 감소하는 부분 수열은 A = {10, 30, 10, 20, 20, 10}  이고, 길이는 3이다.
    입력 : 첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000)
    출력 : 첫째 줄에 수열 A의 가장 긴 감소하는 부분 수열의 길이를 출력한다.

---
+++ BOJ/2022/0313_11722.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	int arr[1001];
+	int dp[1001];
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i <n; i++) {
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (arr[i] < arr[j] && dp[j]+1 > dp[i]) {
+				dp[i] = dp[j] + 1;
+			}
+		}
+	}
+  # dp 배열에서 가장 큰 값 찾기
+	int m = 0;
+	for (int i = 0; i < n; i++) {
+		if (dp[i] > m) {
+			m = dp[i];
+		}
+	}
+	cout << m;
+	return 0;
+}
