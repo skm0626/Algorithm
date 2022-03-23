7f34d7705115e667365d581b0cc58a18b9fcb1c9
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-03-23 23:41:19 +0900

    String, Date. 루시와 엘라 찾기
    동물 보호소에 들어온 동물 중 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty인 동물의 아이디와 이름, 성별 및 중성화 여부를 조회하는 SQL 문을 작성해주세요.

---
+++ Programmers/SQL 고득점 Kit/String, Date_1.sql
@@ -0,0 +1,9 @@
+-- Oracle
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE FROM ANIMAL_INS
+WHERE NAME='Lucy' or NAME='Ella' or NAME='Pickle' or NAME='Rogan' or NAME='Sabrina' or NAME='Mitty'
+ORDER BY ANIMAL_ID
+
+-- MySQL
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE FROM ANIMAL_INS
+WHERE NAME='Lucy' or NAME='Ella' or NAME='Pickle' or NAME='Rogan' or NAME='Sabrina' or NAME='Mitty'
+ORDER BY ANIMAL_ID
