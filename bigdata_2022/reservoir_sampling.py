00c103982d44afac3611dd9e549d9f95a11cf9ea
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-03-31 18:12:06 +0900

    Reservoir sampling Algorithm

---
+++ bigdata_2022/reservoir_sampling.py
@@ -0,0 +1,23 @@
+import random
+class Reservoir:
+    def __init__(self,k): #k개 샘플링하겠다.
+        self.sampled = []
+        self.k = k
+        self.cnt =0 # 몇 번째로 들어온 아이템인지
+
+    def put(self, item): #스트림에서 item 하나가 reservoir sampled로 들어옴
+        if self.cnt < self.k:
+            self.sampled.append(item)
+        else:
+            r = random.randint(0,self.cnt)
+            # randint randrange 차이
+            # randint(0,10) : 0~10 # randrange(0,10) : 0~9
+            if r<self.k:
+                self.sampled[r] = item
+        self.cnt += 1
+
+reservoir = Reservoir(20)
+
+for i in range(1000):
+    reservoir.put(i)
+    print(reservoir.sampled)
