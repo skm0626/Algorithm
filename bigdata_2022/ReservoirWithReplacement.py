2b86fc767bbf0273fd3f7a3a0cc8fb9eff6f0ad9
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-04-13 13:33:30 +0900

    Update ReservoirWithReplacement.py

--- bigdata_2022/ReservoirWithReplacement.py
+++ bigdata_2022/ReservoirWithReplacement.py
@@ -1,4 +1,6 @@
 import random
+import matplotlib.pyplot as plt
+
 class Reservoir:
     def __init__(self,k): #k개 샘플링하겠다.
         self.sampled = []
@@ -6,18 +8,31 @@ class Reservoir:
         self.cnt =0 # 몇 번째로 들어온 아이템인지
 
     def put(self, item): #스트림에서 item 하나가 reservoir sampled로 들어옴
-        if self.cnt < self.k:
+        if self.cnt < self.k: # k개 이하로 들어올 경우 : 배열에 그냥 추가
             self.sampled.append(item)
-        else:
-            r = random.choice(self.cnt+1,1,replace=True)
+        else: # k개 이상이 들어왔을 경우 : r<k일 때만 r번재 값을 item으로 대체
+            r = random.randint(0,self.cnt)
             # randint randrange 차이
             # randint(0,10) : 0~10 # randrange(0,10) : 0~9
             if r<self.k:
                 self.sampled[r] = item
         self.cnt += 1
 
-reservoir = Reservoir(20)
+cnt = [0 for i in range(1000)]
 
-for i in range(1000):
-    reservoir.put(i)
-    print(reservoir.sampled)
+for i in range(10000): #10000
+    reservoir = Reservoir(100)
+    for j in range(1000):
+        reservoir.put(j)
+        print(reservoir.sampled)
+        if j in reservoir.sampled:
+            cnt[j]+=1
+
+
+# 시각화
+x = [i for i in range(1000)]
+y = cnt
+plt.bar(x,y,width=1)
+# plt.set_ylim([0,10000])
+# plt.set(title='Count' ,xlabel='숫자',ylabel='추출횟수')
+plt.show()
