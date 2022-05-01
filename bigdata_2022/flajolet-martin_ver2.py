bea94fdd7f13127d45d30f27b96816651187e010
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-05-01 16:44:56 +0900

    Update and rename flajolet-martin.py to flajolet-martin_ver2.py
    more detail description

--- bigdata_2022/flajolet-martin_ver2.py
+++ bigdata_2022/flajolet-martin_ver2.py
@@ -8,27 +8,26 @@ class FM:
     def __init__(self, domain_size): #들어오는 item이 어느 배열에서 들어오는지 그 배열의 크기
         self.bitarray = 0
         self.domain_size = domain_size
-        #몇개 비트 쓸건지
-        self.n_bits = math.ceil(math.log2(domain_size)) #int로 표현하려고 ceil
-        self.mask = (1<<self.n_bits)-1
-        self.seed = random.randint(0,9999999)
+        self.n_bits = math.ceil(math.log2(domain_size)) #몇개 비트 쓸건지 #int로 표현하려고 ceil
+        self.mask = (1<<self.n_bits)-1 # 아래 put함수에서 hash함수를 통해 나온 값중에 n_bits(개수)만큼만 사용할거니까 필요없는 부분 삭제하기 위해 mask
+        self.seed = random.randint(0,9999999) # hash를 여러개 쓸 수도 있으니까
 
     def put(self,item):
         h = mmh3.hash(item, self.seed) & self.mask
-        r = 0
+        r = 0 # least significant set bit
         if h==0: return
         while (h&(1<<r)==0):
             r+=1
         self.bitarray |= (1<<r) #r번 위치에 1세팅
 
-    def size(self):
+    def size(self): # 개수 세기
         R=0
-        while (self.bitarray & (1<<R) != 0):
+        while (self.bitarray & (1<<R) != 0): # 0이 처음 나온 위치
             R+=1
         return 2**R / 0.77351
 
-fm = FM(1000000)
-tset = set() #true set
+fm = FM(1000000) # 예측한 것
+tset = set() #true set 실제로 집합에 넣은것
 
 x=[]
 y=[]
@@ -41,6 +40,6 @@ for i in tqdm(range(10000)):
     x.append(len(tset))
     y.append(fm.size())
 plt.scatter(x,y)
-plt.plot(x,x,color='r')
+plt.plot(x,x,color='r') # y=x축 그려보기
 plt.show()
     # print(f"true:{len(tset)}, estimated:{fm.size()}")
