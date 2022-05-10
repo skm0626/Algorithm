a2c3dd172226ec910724703e7e52e66ac7806618
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-05-11 00:39:01 +0900

    Add files via upload

--- bigdata_2022/flajolet-martin_ver1.py
+++ bigdata_2022/flajolet-martin_ver1.py
@@ -6,7 +6,6 @@ from tqdm import tqdm
 
 class FM:
     def __init__(self, domain_size): #들어오는 item이 어느 배열에서 들어오는지 그 배열의 크기
-        self.bitarray = 0
         self.domain_size = domain_size
         self.n_bits = math.ceil(math.log2(domain_size)) #몇개 비트 쓸건지 #int로 표현하려고 ceil
         self.mask = (1<<self.n_bits)-1 # 아래 put함수에서 hash함수를 통해 나온 값중에 n_bits(개수)만큼만 사용할거니까 필요없는 부분 삭제하기 위해 mask
@@ -18,17 +17,16 @@ class FM:
         if h==0: return
         while (h&(1<<r)==0):
             r+=1
-        self.bitarray |= (1<<r) #r번 위치에 1세팅
+        arr.append(r)
 
     def size(self): # 개수 세기
-        R=0
-        while (self.bitarray & (1<<R) != 0): # 0이 처음 나온 위치
-            R+=1
-        return 2**R / 0.77351
+        R = max(arr)
+        return 2**R
+
+arr = []
 
 fm = FM(1000000) # 예측한 것
 tset = set() #true set 실제로 집합에 넣은것
-print(fm)
 
 x=[]
 y=[]
@@ -40,6 +38,7 @@ for i in tqdm(range(10000)):
 
     x.append(len(tset))
     y.append(fm.size())
+    # print(arr)
 plt.scatter(x,y)
 plt.plot(x,x,color='r') # y=x축 그려보기
 plt.show()
