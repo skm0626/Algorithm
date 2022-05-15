c4443a29d95186de440303d69e60f90d9625f1f2
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-05-16 03:54:56 +0900

    스택/큐

---
+++ Programmers/CodingTest/다리를 지나는 트럭.py
@@ -0,0 +1,12 @@
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    arr = [0] * bridge_length
+    while len(arr)!=0:
+        answer += 1
+        arr.pop(0)
+        if truck_weights:
+            if sum(arr) + truck_weights[0] <= weight:
+                arr.append(truck_weights.pop(0))
+            else:
+                arr.append(0)
+    return answer
