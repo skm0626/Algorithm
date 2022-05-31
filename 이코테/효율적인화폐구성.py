2d50deafd5e4a6a3cd70d822365eaf02cadfe8e1
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-05-31 23:56:58 +0900

    DP

---
+++ 이코테/효율적인화폐구성.py
@@ -0,0 +1,17 @@
+n,m = map(int,input().split())
+arr = []
+for i in range (n):
+    arr.append(int(input()))
+
+dp = [10001] * (m+1)
+dp[0] = 0
+
+for i in range(n):
+    for j in range(arr[i],m+1):
+        if dp[j-arr[i]]!=10001:
+            dp[j] = min(dp[j],dp[j-arr[i]]+1)
+
+if (dp[m]!=10001):
+    print(dp[m])
+else:
+    print(-1)
