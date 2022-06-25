a21f74bcb3dc405e441ceb5a92c68c8d83984040
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-06-25 23:08:03 +0900

    DP

---
+++ 이코테/금광.py
@@ -0,0 +1,25 @@
+for testcase in range(int(input())):
+    n,m = map(int,input().split())
+    arr = list(map(int,input().split()))
+    dp=[]
+    idx=0
+    for i in range(n):
+        dp.append(arr[idx:idx+m])
+        idx+=m
+    for j in range(1,m):
+        for i in range(n):
+            if i==0:
+                left_up=0
+            else:
+                left_up = dp[i-1][j-1]
+            if i==n-1:
+                left_down = 0
+            else:
+                left_down = dp[i+1][j-1]
+            left = dp[i][j-1]
+            dp[i][j] = dp[i][j] + max(left_up,left,left_down)
+
+    answer=0
+    for i in range(n):
+        answer = max(answer,dp[i][m-1])
+    print(answer)
