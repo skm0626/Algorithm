5962676888277b9376a093f5a3632193a31732ee
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-07-14 00:39:15 +0900

    Add files via upload

---
+++ bigdata_2022/hw0620/task2/TriStep01Mapper.java
@@ -0,0 +1,27 @@
+package task2;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import task2.IntPairWritable;
+
+public class TriStep01Mapper extends Mapper<IntWritable, IntWritable, IntWritable, IntPairWritable>{
+	
+    IntWritable ok1 = new IntWritable();
+	IntPairWritable ov1 = new IntPairWritable();
+	IntWritable ok2 = new IntWritable();
+    IntPairWritable ov2 = new IntPairWritable();
+	
+	@Override
+	protected void map(IntWritable key, IntWritable value, Mapper<IntWritable, IntWritable, IntWritable, IntPairWritable>.Context context)
+        throws IOException, InterruptedException {
+		
+      ov1.set(key.get(), value.get());
+      context.write(key, ov1);
+      ov2.set(key.get(), value.get());
+      context.write(value, ov2);
+	}
+}
