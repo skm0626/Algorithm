93053a6f2a3ccf0e9e20fc4874d8c199f920a976
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-09-27 00:00:54 +0900

    Add files via upload

---
+++ TriStep02Mapper.java
@@ -0,0 +1,25 @@
+package task3;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import task3.IntPairWritable;
+
+public class TriStep02Mapper extends Mapper<IntPairWritable, IntPairWritable, IntPairWritable, IntPairWritable>{
+	
+    IntWritable ok1 = new IntWritable();
+	IntPairWritable ov1 = new IntPairWritable();
+	IntWritable ok2 = new IntWritable();
+    IntPairWritable ov2 = new IntPairWritable();
+	
+	@Override
+	protected void map(IntPairWritable key, IntPairWritable value, Mapper<IntPairWritable, IntPairWritable, IntPairWritable, IntPairWritable>.Context context)
+        throws IOException, InterruptedException {
+		
+//      ov1.set(key.get(), value.get());
+      context.write(key, value);
+	}
+}
