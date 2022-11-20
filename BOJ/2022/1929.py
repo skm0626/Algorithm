4f90824b4c189588af335d888d2fca51d07a9908
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-11-21 03:19:02 +0900

    1929. 소수 구하기
    M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.

---
+++ BOJ/2022/1929.py
@@ -0,0 +1,14 @@
+import math
+
+def isPrime(x):
+    if x==1:
+        return False
+    else:
+        for i in range(2,int(math.sqrt(x))+1):
+            if x%i==0:
+                return False
+        return True
+m,n = map(int,input().split())
+for i in range(m,n+1):
+    if isPrime(i):
+        print(i)
