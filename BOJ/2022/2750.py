d4029b42125d5210a2f21936743757edd218f2ec
kyungmin <39684855+skm0626@users.noreply.github.com>
2022-12-23 04:46:51 +0900

    2750. 수 정렬하기
    N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
    입력
    첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
    출력
    첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.

---
+++ BOJ/2022/2750.py
@@ -0,0 +1,16 @@
+# 버블소트로 구현
+import sys
+n = int(sys.stdin.readline())
+arr = []
+for i in range(n):
+    m = int(sys.stdin.readline())
+    arr.append(m)
+
+for i in range(n-1):
+    for j in range(n-i-1):
+        if arr[j]>arr[j+1]:
+            tmp = arr[j+1]
+            arr[j+1]=arr[j]
+            arr[j]= tmp
+for i in arr:
+    print(i)
